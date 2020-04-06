15fb3c718ad3d77a4d505312a0b3ef13fbe2975a
Ymiii <gui.toniol@gmail.com>
2020-04-06 20:06:01 -0300

    pff

--- comandos/play.js
+++ comandos/play.js
@@ -4,18 +4,6 @@ async function play(client, message){
     link = client.servers.get(message.guild.id).get("fila")[0];      
     client.servers.get(message.guild.id).set("djID", message.member.id);
     let musica = Ytdl(link);
-    
-    Ytdl.getInfo(link, (err, info) => {
-        setTimeout(() => {
-            let title = info.title;
-            let minutes = Math.round(info.length_seconds/60);
-            let seconds = info.length_seconds % 60;
-            minutes = minutes < 10? '0' + minutes : ''+minutes;
-            seconds = seconds < 10? '0' + seconds : ''+seconds;
-            
-            message.channel.send(`:musical_note: Tocando **${title} (${minutes}:${seconds})** adicionado por **${message.member.user.username}**`);           
-        }, 1000);
-    })
 
     message.member.voiceChannel.connection.playStream(musica).on('end', () => {
         client.servers.get(message.guild.id).get("fila").shift();            
@@ -55,17 +43,19 @@ module.exports = {
         fila = await client.servers.get(message.guild.id).get("fila");
         await fila.push(args[0]);
         
-        if(fila.length > 1){
-            Ytdl.getInfo(args[0], (err, info) => {
-                let title = info.title;
-                let minutes = Math.round(info.length_seconds/60);
-                let seconds = info.length_seconds % 60;
-                minutes = minutes < 10? '0' + minutes : ''+minutes;
-                seconds = seconds < 10? '0' + seconds : ''+seconds;
+        Ytdl.getInfo(args[0], (err, info) => {
+            let title = info.title;
+            let minutes = Math.round(info.length_seconds/60);
+            let seconds = info.length_seconds % 60;
+            minutes = minutes < 10? '0' + minutes : ''+minutes;
+            seconds = seconds < 10? '0' + seconds : ''+seconds;
 
+            if(fila.length > 1) 
                 message.channel.send(`:white_check_mark: **${title} (${minutes}:${seconds})** foi adicionado a fila com sucesso!`);
-            });            
-        }
+            else
+                message.channel.send(`:musical_note: Tocando **${title} (${minutes}:${seconds})** adicionado por **${message.member.user.username}**`);
+        });            
+        
     
         client.servers.get(message.guild.id).set("tocando", true);
         if(fila.length === 1) 
