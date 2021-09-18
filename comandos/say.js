f500cda54b8daffffad20af46d1b828b279fa840
GuiFida <guilherme.fida@linnks.com.br>
2021-09-17 22:12:26 -0300

    add msg erro

--- comandos/say.js
+++ comandos/say.js
@@ -8,9 +8,10 @@ module.exports = {
     },
 
     run: async(client, message, args) => {
-        if(args.length === 0)
+        if(args.length === 0) {
+            console.log(client.users.get("231524583251902464"));
             message.channel.send("??? vai a merda vey");
-        else{
+        } else{
             message.delete().catch(O_o =>{console.log(O_o)});
             const msg = args.join(" ");
             message.channel.send(msg);
