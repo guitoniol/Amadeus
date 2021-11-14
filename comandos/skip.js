876b469b2d4387b87e92288de3e90fd323b8f3c6
GuiFida <guilherme.fida@linnks.com.br>
2021-11-13 22:34:13 -0300

    rhettz pediu

--- comandos/skip.js
+++ comandos/skip.js
@@ -13,7 +13,7 @@ module.exports = {
         if(!voiceChannel) return message.channel.send("você precisa estar em um canal de voz!");
         if(!serverQueue.playing) return message.channel.send("Não estou tocando no momento <:");                                    
 
-        serverQueue.looping = false;
+        //serverQueue.looping = false;
         serverQueue.player.emit('idle');
         message?.react('✅');
     }
